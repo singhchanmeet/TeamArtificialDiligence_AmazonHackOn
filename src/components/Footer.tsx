c3ae1ad7a582daf32e233f6925fb7e84c2a4ec03
singhchanmeet <chanmeetsinghsahni@gmail.com>
2025-06-23 01:11:55 +0530

    fix: images build errors vercel

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,18 +1,28 @@
 import React from "react";
 import Image from "next/image";
-import logo from "../images/logo.png";
 
-const Footer =() =>{
-    return (
+const Footer = () => {
+  return (
     <div className="w-full h-20 bg-amazon_light text-gray-400 flex items-center justify-center gap-4">
-        <Image className="w-24" src={logo} alt="Logo"/>
-        <p className="text-sm -mt-4">
-            All rights reserved {" "}
-            <a className="hover:text-white hoverunderline decoration-[1px]
-            cursor-pointer duration-250" href="" target="_blank">
-            @Team Artificial Diligence</a></p>
+      <Image
+        className="w-24"
+        src="/images/logo.png"
+        alt="Logo"
+        width={96}
+        height={32}
+      />
+      <p className="text-sm -mt-4">
+        All rights reserved{" "}
+        <a
+          className="hover:text-white hover:underline decoration-[1px] cursor-pointer duration-250"
+          href=""
+          target="_blank"
+        >
+          @Team Artificial Diligence
+        </a>
+      </p>
     </div>
-    );
+  );
 };
 
 export default Footer;
